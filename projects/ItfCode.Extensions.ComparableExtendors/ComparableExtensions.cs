54577f187e5df0a2d5414c3ac9d43c7e4aee7bc3
Vince Jacobs <vince.jacobs@bbconsult.co.uk>
2022-07-06 18:35:24 +0300

    Commit 06

--- projects/ItfCode.Extensions.ComparableExtendors/ComparableExtensions.cs
+++ projects/ItfCode.Extensions.ComparableExtendors/ComparableExtensions.cs
@@ -3,13 +3,13 @@
     public static class ComparableExtensions
     {
         public static bool Within<T>(this T self, T start, T finish) where T : IComparable, IComparable<T>
-            => self != null ? self.CompareTo(start) >= 0 && self.CompareTo(finish) <= 0 : throw new ArgumentNullException(nameof(self));
+            => self != null ? Predicate(self).Invoke((start, finish)) : throw new ArgumentNullException(nameof(self));
 
         public static bool WithinAny<T>(this T self, params (T, T)[] values) where T : IComparable, IComparable<T>
-            => self != null ? values.Any(x => self.CompareTo(x.Item1) >= 0 && self.CompareTo(x.Item2) <= 0) : throw new ArgumentNullException(nameof(self));
+            => self != null ? values.Any(Predicate(self)) : throw new ArgumentNullException(nameof(self));
 
         public static bool WithinAll<T>(this T self, params (T, T)[] values) where T : IComparable, IComparable<T>
-            => self != null ? values.All(x => self.CompareTo(x.Item1) >= 0 && self.CompareTo(x.Item2) <= 0) : throw new ArgumentNullException(nameof(self));
+            => self != null ? values.All(Predicate(self)) : throw new ArgumentNullException(nameof(self));
 
         public static bool Without<T>(this T self, T start, T finish) where T : IComparable, IComparable<T>
             => !self.Within(start, finish);
@@ -19,5 +19,8 @@
 
         public static bool WithoutAll<T>(this T self, params (T, T)[] values) where T : IComparable, IComparable<T>
             => !self.WithoutAll(values);
+
+        private static Func<(T, T), bool> Predicate<T>(T value) where T : IComparable, IComparable<T>
+            => (x) => value.CompareTo(x.Item1) >= 0 && value.CompareTo(x.Item2) <= 0;
     }
 }
